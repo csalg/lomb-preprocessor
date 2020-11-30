2d82260f86dbabfc3169f9744cd918d4070a3e30
Charlie Salgado <charlie@csalgado.io>
2020-11-30 01:42:49 +0100

    Updated vtt serializer to output track with lemmas

--- src/serial/serializers/VttSerializer.py
+++ src/serial/serializers/VttSerializer.py
@@ -1,4 +1,5 @@
-from copy import copy
+from copy import copy, deepcopy
+from os.path import splitext
 
 import webvtt
 
@@ -8,31 +9,57 @@ from .SerializerABC import SerializerABC
 from serial.util import convert_to_key, format_caption
 
 class VttSerializer(SerializerABC):
-    def __init__(self, filename):
+
+    def __init__(self, filename, source_language, target_language):
+        super().__init__(filename, source_language, target_language)
         fix_standalone_cues(filename)
         convert_to_utf8(filename)
+
+        self.source_language = source_language
+        self.target_language = target_language
         self.source = webvtt.read(filename)
-        self.chunks_dictionary = {}
+        self.translation_dictionary = {}
+        self.lemmas_dictionary = {}
 
         for caption in self.source.captions:
-            self.chunks_dictionary[convert_to_key(caption.text)] = ''
+            key = convert_to_key(caption.text)
+            self.translation_dictionary[key] = ''
+            self.lemmas_dictionary[key] = ''
 
-    def get_dictionary(self):
-        return self.chunks_dictionary
-    
-    def set_dictionary(self, new_dictionary):
-        self.chunks_dictionary = new_dictionary
+    def get_translation_dictionary(self):
+        return self.translation_dictionary
+
+    def set_translation_dictionary(self, new_dictionary):
+        self.translation_dictionary = new_dictionary
+
+    def get_lemmas_dictionary(self):
+        return self.lemmas_dictionary
+
+    def set_lemmas_dictionary(self, new_dictionary):
+        self.lemmas_dictionary = new_dictionary
 
     def save_to_file(self, output_filename):
-        translation = copy(self.source)
-        for caption in translation:
-            print(caption)
-            new_text = self.chunks_dictionary[convert_to_key(caption.text)]
-            print(new_text)
-            new_text_formatted = format_caption(new_text)
-            caption.text = new_text_formatted
-        print('finished')
+        print('Saving')
+        translation = self.__translate_source(self.translation_dictionary)
+        lemmas = self.__translate_source(self.lemmas_dictionary, format_captions=False)
         translation.save(output_filename)
+        lemmas_filename = splitext(output_filename)[0] + '.lemmas.' + splitext(output_filename)[1]
+        lemmas.save(lemmas_filename)
+
+    def __translate_source(self, dictionary, format_captions = True):
+        translation = deepcopy(self.source)
+        print(dictionary)
+        for caption in translation:
+            key = convert_to_key(caption.text)
+            new_text = dictionary[key] if key in dictionary else ""
+            if type(new_text) == dict:
+                new_text = ','.join(new_text.values())
+            if format_captions:
+                new_text = format_caption(new_text)
+            print(key, new_text)
+            caption.text = new_text
+        return translation
+
 
 def fix_standalone_cues(filename):
     lines = []
