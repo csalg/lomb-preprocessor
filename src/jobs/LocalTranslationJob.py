e59df61c3de9e7456090b6ae8ac4dec7a12f8c9c
Carlos Salgado <charlie@csalgado.io>
2021-02-24 18:52:22 +0100

    Added call to Google Translate for Danish texts

--- src/jobs/LocalTranslationJob.py
+++ src/jobs/LocalTranslationJob.py
@@ -1,11 +1,13 @@
+import os
 from os.path import splitext
 
-# from jobs.TranslationJobABC import TranslationJob
+from termcolor import colored
+
+from util import LombPreprocessorException
+from logging_ import logger
 from serial import SerializerSimpleFactory
 from taggers.createTagger import createTagger
-from translators.selenium_translators import GoogleTranslator
-from translators_old.DeepLTranslator import DeepLTranslator
-from translators_old.TranslatorFactory import create_translator
+from translators.selenium_translators import GoogleTranslator, DeepLTranslator
 from translators_old.util import infer_language
 from util import parse_title_source_language_and_extension_from_filename
 
@@ -15,25 +17,30 @@ class LocalTranslationJob():
         self.source_language        = self._deduce_source_language(input_filename, source_language)
         self.target_language        = self._deduce_target_language(target_language)
         self.title, self.extension  = self._split_filename_into_title_and_extension(input_filename)
+        if os.path.isfile(self.output_filename()):
+            raise FileExistsException(f'{self.output_filename()} found in directory. Skipping')
 
         serializer_constructor = SerializerSimpleFactory.create(input_filename)
         self.serializer = serializer_constructor(input_filename, self.source_language, self.target_language)
         self.tagger = createTagger(self.source_language)()
-        self.translator = DeepLTranslator(self.source_language, self.target_language)
+        if self.source_language in ['dk', 'da']:
+            self.translator = GoogleTranslator('da', self.target_language)
+        else:
+            self.translator = DeepLTranslator(self.source_language, self.target_language)
 
     def run(self):
-        print(f'Executing: {self.output_filename()}')
+        logger.info(f'{colored(self.title + "." + self.extension, "yellow")} will be translated from {colored(self.source_language, "red")} to {colored(self.target_language, "red")}')
 
         translation_dictionary = self.serializer.get_translation_dictionary()
         if self.source_language != self.target_language:
             translation_dictionary = self.translator.translate(translation_dictionary)
+        self.serializer.set_translation_dictionary(translation_dictionary)
 
         lemmas_dictionary = self.serializer.get_lemmas_dictionary()
         lemmas_dictionary = self.tagger.tag(lemmas_dictionary)
 
-        self.serializer.set_translation_dictionary(translation_dictionary)
         self.serializer.set_lemmas_dictionary(lemmas_dictionary)
-        print("Translation finished")
+        logger.info(colored("Translation finished", 'green'))
         self.serializer.save_to_file(self.output_filename())
 
     def _deduce_source_language(self, input_filename, given_source_language):
@@ -55,113 +62,13 @@ class LocalTranslationJob():
         return title, ext[1:]
 
 
-# class RemoteTranslationJob(TranslationJob):
-    # def __init__(self,*args, **kwargs):
-    #     super().__init__(*args, **kwargs)
-    #
-    # def run(self):
-    #     pass
-    #
+class FileExistsException(LombPreprocessorException):
+    pass
 
 def run_local_translation_job_from_filename(source_language, target_language, input_filename, input_json=None):
-    job = LocalTranslationJob(source_language, target_language, input_filename, input_json)
-    job.run()
+    try:
+        job = LocalTranslationJob(source_language, target_language, input_filename, input_json)
+        job.run()
+    except LombPreprocessorException as e:
+        logger.warning(colored(e, 'blue'))
 
-# def run_local_translation_job_from_filename(input_filename, given_source_language, target_language, input_json=None):
-#
-#     source_language = deduce_source_language(input_filename, given_source_language)
-#     target_language = deduce_target_language(target_language)
-#     title, extension = split_filename_into_title_and_extension(input_filename)
-#     output_filename = make_output_filename(title, extension)
-#
-#     if not is_recognized_language(target_language):
-#         raise Exception(f"Target language '{target_language}' is not recognized.")
-#
-#     if given_source_language:
-#         source_language = given_source_language
-#         title, extension = os.path.splitext(input_filename)
-#         extension = extension[1:]
-#     else:
-#         title, source_language, extension = parse_title_source_language_and_extension_from_filename(input_filename)
-#
-#     inferred_source_language = infer_language(source_language)
-#
-#     output_filename = f'{title}.{target_language}.{extension}'
-#     print(output_filename)
-#
-#     if not is_recognized_extension(extension):
-#         raise Exception(f"Extension '{extension}' is not recognized.")
-#
-#     if not is_recognized_language(inferred_source_language):
-#         raise Exception(f"Source language '{source_language}' is not recognized.")
-#
-#     if extension != 'epub':
-#         convert_to_utf8(input_filename)
-#         get_rid_of_empty_lines_after_time_in_vtt(input_filename)
-#
-#     if requires_conversion(extension):
-#         print('converting')
-#         new_filename = convert(input_filename)
-#         return run_local_translation_job_from_filename(new_filename, given_source_language, target_language)
-#
-#     if os.path.exists(output_filename) or os.path.exists(output_filename+'.json'):
-#         raise Exception(f"Translated file '{output_filename}' already exists")
-#
-#     serializer_constructor = serializer_from_extension(extension)
-#     translator_constructor = DeepLTranslator
-#
-#     serializer = serializer_constructor(input_filename)
-#     if input_json:
-#         previous_translation_dictionary = get_dictionary_from_json(input_json)
-#         serializer.set_dictionary(previous_translation_dictionary)
-#     translator = translator_constructor(inferred_source_language, target_language)
-#
-#     job = LocalTranslationJob(output_filename, serializer, translator)
-#     job.run()
-#
-#
-# def run_package_translation_job(given_source_language, target_language, filenames, input_json=None):
-#
-#     if not is_recognized_language(target_language):
-#         raise Exception(f"Target language '{target_language}' is not recognized.")
-#
-#     if not given_source_language:
-#         raise Exception('Cannot infer source language for packaged texts. Please provide a source language.')
-#
-#     metadata = get_metadata(filenames[0])
-#     output_filename = f'{metadata["title"]}.{target_language}'
-#     print(output_filename)
-#
-#     inferred_source_language = infer_language(given_source_language)
-#     if not is_recognized_language(inferred_source_language):
-#         raise Exception(f"Source language '{inferred_source_language}' is not recognized.")
-#
-#     for filename in filenames:
-#         convert_to_utf8(filename)
-#
-#     if os.path.exists(output_filename) or os.path.exists(output_filename+'.json'):
-#         raise Exception(f"Translated file '{output_filename}' already exists")
-#
-#     metadata['source_language'] = inferred_source_language
-#     metadata['support_language'] = target_language
-#     serializer = TextPackageSerializer(metadata, filenames)
-#     translator_constructor = DeepLTranslator
-#
-#     if input_json:
-#         previous_translation_dictionary = get_dictionary_from_json(input_json)
-#         serializer.set_dictionary(previous_translation_dictionary)
-#     translator = translator_constructor(inferred_source_language, target_language)
-#
-#     job = LocalTranslationJob(output_filename, serializer, translator)
-#     job.run()
-#
-#
-# @celery.task
-# def run_local_translation_job_from_filename_celery_task(*args, **kwargs):
-#     """     try:
-#         run_local_translation_job_from_filename(*args, **kwargs)
-#     except Exception as e:
-#         print(e) """
-#     run_local_translation_job_from_filename(*args, **kwargs)
-#
-#
