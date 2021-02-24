b38b12d36808fa9c175c83a822fc36ba6cf15244
Carlos Salgado <charlie@csalgado.io>
2021-02-24 18:26:28 +0100

    Completely rewrote translators module

--- src/translators_old/TranslatorFactory.py
+++
@@ -1,5 +0,0 @@
-from translators_old.DeepLTranslator import DeepLTranslator, ChineseInteractionAgent
-
-
-def create_translator(source_language, target_language):
-    return DeepLTranslator(source_language,target_language,ChineseInteractionAgent)
