e752c441642bd835479c78c9ca781d1ec0d1c361
Carlos Salgado <charlie@csalgado.io>
2021-02-24 18:54:14 +0100

    Set up custom logger (before was using root logger and was very noisy)

--- src/logging_.py
+++ src/logging_.py
@@ -6,4 +6,3 @@ from config import LOGGING_LEVEL
 logger = logging.getLogger('lomb_preprocessor')
 logger.addHandler(logging.StreamHandler(sys.stdout))
 logger.setLevel(LOGGING_LEVEL)
-logger.debug('Logger works')
